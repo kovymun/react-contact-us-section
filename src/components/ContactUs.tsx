7fa0621b0871449491266bdc2025464844554cdd
Kovy <codingstudent2021@gmail.com>
2025-04-22 14:11:02 +0200

    Adding Initial Content Structure

---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,24 @@
+function ContactUs() {
+  return (
+    <div className="max-w-content flex flex-col justify-center items-center">
+      <div
+        id="location"
+        className="location bg-[#81E7AF] p-6 text-[#2e2e2e] font-extrabold text-4xl outline-2"
+      >
+        <p>Color Test</p>
+      </div>
+
+      {/* Location */}
+      <div id="location" className="location">
+        <h2 className="text-xl font-bold">Location</h2>
+        <p>Riverside Medical Centre</p>
+        <p>102 Willowbend Road</p>
+        <p>Westwood, Cape Town 7700</p>
+      </div>
+
+      {/* Contact Details */}
+    </div>
+  );
+}
+
+export default ContactUs;
