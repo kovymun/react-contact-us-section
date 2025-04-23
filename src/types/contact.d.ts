8e51a4a650ba5a3358b6ec21db1b82b9313235a7
Kovy <codingstudent2021@gmail.com>
2025-04-23 17:49:30 +0200

    Loading location data from simulated DB instead of hardcoded values

--- src/types/contact.d.ts
+++ src/types/contact.d.ts
@@ -1,12 +1,19 @@
 //storing all interface types
 
 //Interface for the medi-centre DB
-export interface MediCentreHours {
+interface MediCentreHours {
   id: string;
   title: string;
   hour_content: string[];
 }
 
+interface MediCentreLocation {
+  id: string;
+  title: string;
+  loc_content: MediCentreLocationDetails[];
+}
+
 export interface MediCentreDB {
   hours: MediCentreHours[];
+  location: MediCentreLocation[];
 }
