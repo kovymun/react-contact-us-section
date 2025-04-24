76030a5d46abde2a06e11af5a1a3873336a75287
Kovy <codingstudent2021@gmail.com>
2025-04-24 20:59:26 +0200

    Adding react-hook-form validation

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,15 +1,46 @@
 import { Clock, MapPin, Phone, MessageCircleMore } from "lucide-react";
 import { MediCentreDB } from "../types/contact";
 import { v4 as uuidv4 } from "uuid";
+import { useForm } from "react-hook-form";
 
 // Step 1: Define props interface
 interface ContactUsProps {
   data: MediCentreDB;
 }
 
+type AppointmentFormInputs = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  practitioner: string;
+  preferredTime: string;
+  reason: string;
+};
+
 function ContactUs({ data }: ContactUsProps) {
   // Destructuring data
   const { hours, location, contact } = data;
+
+  // useForm hook
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<AppointmentFormInputs>();
+
+  const emailRegexPattern = new RegExp(
+    /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/
+  );
+
+  const mobileRegexPattern = new RegExp(/^[0-9]{10}$/);
+
+  const reqAppointment = () => {
+    reset();
+    console.log("TEST");
+  };
+
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center font-sans">
       {/* Contact Us Section Intro */}
@@ -140,7 +171,11 @@ function ContactUs({ data }: ContactUsProps) {
           <div className="modal-action">
             <div className="w-full">
               {" "}
-              <form method="dialog" className="grid grid-cols-4 gap-2">
+              <form
+                method="dialog"
+                onSubmit={handleSubmit(reqAppointment)}
+                className="grid grid-cols-4 gap-2"
+              >
                 {/* Daisy UI btn */}
                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                   ✕
@@ -148,7 +183,10 @@ function ContactUs({ data }: ContactUsProps) {
 
                 {/* Patient Name */}
                 <div className="col-span-4">
-                  <label className="block mb-1 font-medium text-sm">
+                  <label
+                    htmlFor="firstName"
+                    className="block mb-1 font-medium text-sm"
+                  >
                     Patient Name
                   </label>
                   <div className="flex gap-2 bg-white">
@@ -156,35 +194,86 @@ function ContactUs({ data }: ContactUsProps) {
                       type="text"
                       placeholder="First"
                       className="block w-full px-4 py-2 text-[#2e2e2e] bg-transparent border border-gray-300 placeholder-gray-400 placeholder:text-sm focus:outline-none leading-relaxed"
+                      {...register("firstName", {
+                        required: {
+                          value: true,
+                          message: "Patient first name is required",
+                        },
+                      })}
                     />
+                    {errors.firstName && (
+                      <p className="grid">{errors.firstName.message}</p>
+                    )}
                     <input
                       type="text"
                       placeholder="Last"
                       className="block w-full px-4 py-2 text-[#2e2e2e] bg-transparent border border-gray-300 placeholder-gray-400 placeholder:text-sm focus:outline-none leading-relaxed"
+                      {...register("lastName", {
+                        required: {
+                          value: true,
+                          message: "Patient last name is required",
+                        },
+                      })}
                     />
+                    {errors.lastName && (
+                      <p className="grid">{errors.lastName.message}</p>
+                    )}
                   </div>
                 </div>
 
                 {/* Patient Contact Info */}
                 <div className="col-span-2 mt-2">
-                  <label className="block mb-1 font-medium text-sm">
+                  <label
+                    htmlFor="number"
+                    className="block mb-1 font-medium text-sm"
+                  >
                     Contact Phone
                   </label>
                   <input
                     type="text"
                     placeholder="Phone"
                     className="block w-full px-4 py-2 text-[#2e2e2e] bg-transparent border border-gray-300 placeholder-gray-400 placeholder:text-sm focus:outline-none leading-relaxed"
+                    maxLength={10}
+                    {...register("phone", {
+                      required: {
+                        value: true,
+                        message: "Mobile number is required",
+                      },
+                      pattern: {
+                        value: mobileRegexPattern,
+                        message: "Please enter a valid mobile number",
+                      },
+                      minLength: {
+                        value: 10,
+                        message: "Mobile number should be at least 10 digits",
+                      },
+                    })}
                   />
+                  {errors.phone && <p className="">{errors.phone.message}</p>}
                 </div>
                 <div className="col-span-2 mt-2">
-                  <label className="block mb-1 font-medium text-sm">
+                  <label
+                    htmlFor="email"
+                    className="block mb-1 font-medium text-sm"
+                  >
                     Contact Email
                   </label>
                   <input
                     type="email"
                     placeholder="Email"
                     className="block w-full px-4 py-2 text-[#2e2e2e] bg-transparent border border-gray-300 placeholder-gray-400 placeholder:text-sm focus:outline-none leading-relaxed"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Email address is required",
+                      },
+                      pattern: {
+                        value: emailRegexPattern,
+                        message: "Please enter a valid email address",
+                      },
+                    })}
                   />
+                  {errors.email && <p className="">{errors.email.message}</p>}
                 </div>
 
                 {/* Who to See */}
@@ -192,8 +281,8 @@ function ContactUs({ data }: ContactUsProps) {
                   <label className="block mb-1 font-medium text-sm">
                     Who do you need to see?
                   </label>
-                  <select className="px-4 py-2 w-full border border-gray-300 text-sm mt-1">
-                    <option className="text-gray-400 text-sm" disabled selected>
+                  <select className="px-4 py-2 w-full border border-gray-300 text-sm mt-1" defaultValue="Select a practitioner">
+                    <option className="text-gray-400 text-sm" disabled>
                       Select a practitioner
                     </option>
 
@@ -237,8 +326,13 @@ function ContactUs({ data }: ContactUsProps) {
                   ></textarea>
                 </div>
 
-                <button className="col-span-4 mt-4 bg-[#2c786c] text-white px-4 py-2 font-semibold cursor-pointer tracking-wide uppercase">
-                  Request Appointment
+                <button
+                  type="submit"
+                  className="col-span-4 mt-4 bg-[#2c786c] text-white px-4 py-2 font-semibold cursor-pointer tracking-wide uppercase"
+                >
+                  {isSubmitting
+                    ? "Confirming Appointment..."
+                    : "Request Appointment"}
                 </button>
               </form>
             </div>
