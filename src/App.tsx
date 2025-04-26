1132ab38ee51816540fea14c2815e8e46bd60e69
Kovy <codingstudent2021@gmail.com>
2025-04-26 16:51:20 +0200

    Adding toast-messages with react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import ContactUs from "./components/ContactUs";
 import { mediCentreDB } from "./data/medi-centre-db";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <main>
       <ContactUs data={mediCentreDB} />
+      <Toaster position="top-right" />
     </main>
   );
 }
