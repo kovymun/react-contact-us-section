8de71c8a1e239359729934438092324220405849
Kovy <codingstudent2021@gmail.com>
2025-05-01 19:09:38 +0200

    Refining code comments

--- src/types/contact.d.ts
+++ src/types/contact.d.ts
@@ -1,6 +1,6 @@
-//storing all interface types
+// Storing all interface types for the MediCentre database
 
-//Interface for the medi-centre DB
+// Interface representing the hours of operation for the medical centre
 interface MediCentreHours {
   id: string;
   title: string;
@@ -10,24 +10,28 @@ interface MediCentreHours {
   }[];
 }
 
+// Interface representing the location information of the medical centre
 interface MediCentreLocation {
   id: string;
   title: string;
   loc_content: MediCentreLocationDetails[];
 }
 
+// Interface representing the contact information of the medical centre
 interface MediCentreContact {
   id: string;
   title: string;
   contact_content: MediCentreContactDetails[];
 }
 
+// Interface representing a list of practitioners at the medical centre
 interface MediCentrePractitioners {
   practice_id: string;
   doctor: string;
   medicalField: string;
 }
 
+// Interface for the entire MediCentre database
 export interface MediCentreDB {
   hours: MediCentreHours[];
   location: MediCentreLocation[];
